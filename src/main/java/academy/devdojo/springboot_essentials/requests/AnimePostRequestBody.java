acdd37df26cf7abfa959b77cbef40526b9264787
João Victor <profissionaljoao1101@gmail.com>
2025-06-12 20:30:16 -0300

    feat: Incluindo o restante dos testes de integração e incluindo maven profile para personalização da execução dos testes unitários/integração via mvn test.

--- src/main/java/academy/devdojo/springboot_essentials/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot_essentials/requests/AnimePostRequestBody.java
@@ -2,11 +2,15 @@ package academy.devdojo.springboot_essentials.requests;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnimePostRequestBody {
     //validações de campos com Jakarta validation
     @NotEmpty(message = "The anime name cannot be empty")
